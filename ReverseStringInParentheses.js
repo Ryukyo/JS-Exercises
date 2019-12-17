02f89ec395423ad970cb1c1b5462628dcb738032
Florian Doitsukara <florian.fejer@hotmail.de>
2019-12-17 22:47:24 +0100

    Reverse chars in parenthesis with recusion and regex

---
+++ ReverseStringInParentheses.js
@@ -0,0 +1,17 @@
+function reverseInParentheses(inputString) {
+    if (inputString.includes('(')){
+        return reverseInParentheses(reverse(inputString));  //recursive function call as long as an opening bracket is found
+    } 
+    return inputString
+}
+
+function reverse(inputString) {
+    let regex = /\(([^()]*)\)/i; //case sensitive matches of the "words" within brackets
+    let subString = regex.exec(inputString)[1];  //exec executes a search and returns an array
+
+    subString = subString.split("").reverse().join("");
+    return inputString.replace(regex, subString);
+}
+
+console.log(reverseInParentheses("foo(bar)baz(blim)"))  //foorabbazmilb
+console.log(reverseInParentheses("foo(bar(baz))blim")) //foobazrabblim
