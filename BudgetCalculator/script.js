d1123285f5cf561d49ba50f5f0a6d8f3e9f9179e
Florian Doitsukara <florian.fejer@hotmail.de>
2020-01-12 22:06:10 +0100

    Create structure, HTML strings and methods to get user input

---
+++ BudgetCalculator/script.js
@@ -0,0 +1,80 @@
+let budgetController = (function() {
+    
+})();
+
+
+let UIController = (function() {
+    
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
+    };
+       
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //'inc'-ome or 'exp'-ense
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+            };
+        },        
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+let controller = (function(budgetCtrl, UICtrl) {
+    
+    let setupEventListeners = function() {
+        let DOM = UICtrl.getDOMstrings();
+        
+        //Click on button or enter to accept user input
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });        
+    };
+
+    var ctrlAddItem = function() {
+        var input, newItem;
+        
+        // Get the user input data
+        input = UICtrl.getInput();        
+        
+        /* if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+            
+            // 6. Calculate and update percentages
+            updatePercentages();
+        } */
+    };
+
+})(budgetController, UIController);
