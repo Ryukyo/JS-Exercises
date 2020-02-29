84059d700fd871fb23e21672263ee8c7ef1aeb0b
Florian Doitsukara <florian.fejer@hotmail.de>
2020-02-29 23:09:01 +0100

    EJS Ch.6 - 3

--- ClassExercises.js
+++ ClassExercises.js
@@ -53,14 +53,43 @@ class Group {
         }
         return group;
     };
+
+    [Symbol.iterator] () {
+        return new GroupIterator (this);
+    };
 };
 
-let group = Group.from([10, 20]);
-console.log(group.has(10));
+
+class GroupIterator {
+    constructor (group) {
+        this.group = group;
+        this.position = 0;
+    }
+
+    next() {
+        if (this.position >= this.group.valueGroup.length) {
+            return {done: true}
+        } else {
+            let result = {
+               value: this.group.valueGroup[this.position], done: false
+            };
+            this.position++;
+            return result;
+        }
+    }
+}
+
+//let group = Group.from([10, 20]);
+//console.log(group.has(10));
 // → true
-console.log(group.has(30));
+//console.log(group.has(30));
 // → false
-group.add(10);
-group.delete(10);
-console.log(group.has(10));
+//group.add(10);
+//group.delete(10);
+//console.log(group.has(10));
 // → false
+
+for (let value of Group.from(["a", "b", "c"])) {
+    console.log(value);
+}
+// a, b, c
