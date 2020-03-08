51d390b5af841d07c470dc1ffb2cfa9e90405a38
Florian Doitsukara <florian.fejer@hotmail.de>
2020-03-08 23:13:21 +0100

    Add axios and try API calls

--- CookingAssistant/src/js/index.js
+++ CookingAssistant/src/js/index.js
@@ -1,4 +1,12 @@
 // Global app controller
-import test from './test'
-const x = 100;
-console.log(`Imported ${test}. Variable x equals ${x}`);
+import axios from 'axios';
+
+async function getResults (query) {
+    try {
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = result.data.recipes;
+        return recipes;
+    } catch (error) {
+        console.log(error);
+    }
+}
