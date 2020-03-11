0d1e2dba575e6a67d1432bef626a712986536c00
Florian Doitsukara <florian.fejer@hotmail.de>
2020-03-11 22:36:12 +0100

    Create search module and test it

--- CookingAssistant/src/js/index.js
+++ CookingAssistant/src/js/index.js
@@ -1,12 +1,6 @@
 // Global app controller
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults (query) {
-    try {
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = result.data.recipes;
-        return recipes;
-    } catch (error) {
-        console.log(error);
-    }
-}
+const search = new Search('pizza');
+
+search.getResults()
