26d136afd3799484710126a9f11d72fafeffe7d1
Florian Doitsukara <florian.fejer@hotmail.de>
2020-03-14 22:38:23 +0100

    Prepare search controller

--- CookingAssistant/src/js/index.js
+++ CookingAssistant/src/js/index.js
@@ -1,6 +1,26 @@
 // Global app controller
 import Search from './models/Search';
+const state = {};
 
-const search = new Search('pizza');
+const controlSearch = async () => {
+    //Get query from view TODO
+    const query = 'pizza';
 
-search.getResults()
+    if(query) {
+        //Create and add search object to state
+        state.search = new Search(query);
+
+        //Clear input and loading indicator
+
+        //Search for recipes
+        await state.search.getResults();
+
+        //Display results
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
