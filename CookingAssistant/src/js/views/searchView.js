c522dd244d6b953e56eecfee4b2e4ea7676f2a85
Florian Doitsukara <florian.fejer@hotmail.de>
2020-03-19 22:30:51 +0100

    Add functionality to limit number of shown results and prepare button logic

--- CookingAssistant/src/js/views/searchView.js
+++ CookingAssistant/src/js/views/searchView.js
@@ -2,8 +2,12 @@ import {elements} from './base';
 
 export const getInput = () => elements.searchInput.value;
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page = 1, resultsPerPage = 10) => {
+    // Show results of the received array 0-9 on page 1, 10 - 19 on page 2 ...
+    const start = (page - 1) - resultsPerPage;
+    const end = page * resultsPerPage;
+    // Slice excludes end
+    recipes.slice(start, end).forEach(renderRecipe);
 };
 
 export const clearInput = () => {
@@ -12,6 +16,35 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResultList.innerHTML = '';
+};
+
+const createButton = (page, buttonType) => `<!--
+    <button class="btn-inline results__btn--prev">
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-left"></use>
+        </svg>
+        <span>Page 1</span>
+    </button>
+    <button class="btn-inline results__btn--next">
+        <span>Page 3</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-right"></use>
+        </svg>
+    </button>
+    -->
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    // Round up in case of decimal "pages" value
+    const pages = Math.ceil(numResults / resPerPage);
+
+    if (page === 1 && pages > 1) {
+        // Only button for "next" page and if more than one page
+    } else if (page < pages) {
+        // Display "next" and "previous"
+    } else if (page === pages && pages > 1) {
+        // On last page only show "previous" button
+    }
 };
 
 const renderRecipe = recipe => {
