4664507b72460410d96d69b216dd120a216e5282
Ryukio <florian.fejer@hotmail.de>
2020-04-15 23:05:34 +0200

    Initial commit of simple node.js entry project

---
+++ LaptopStore/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((request, response) => {
+
+    const pathName = url.parse(request.url, true).pathname;
+    const id = url.parse(request.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '/') {
+        response.writeHead(200, { 'Content-type' : 'text/html' });
+        response.end('Products');
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        response.writeHead(200, { 'Content-type' : 'text/html' });
+        response.end(`This is the laptop with id ${id}`);
+    } else {
+        response.writeHead(404, { 'Content-type' : 'text/html' });
+        response.end('Page not found');
+    }
+
+    
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests');
+});
