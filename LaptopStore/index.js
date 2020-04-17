e6d634638a6bca037745ab5d1db38e5eb7723d24
Ryukio <florian.fejer@hotmail.de>
2020-04-17 23:25:16 +0200

    Finishing exercise

--- LaptopStore/index.js
+++ LaptopStore/index.js
@@ -10,20 +10,58 @@ const server = http.createServer((request, response) => {
     const pathName = url.parse(request.url, true).pathname;
     const id = url.parse(request.url, true).query.id;
 
+    // Products overview
     if (pathName === '/products' || pathName === '/') {
         response.writeHead(200, { 'Content-type' : 'text/html' });
-        response.end('Products');
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                response.end(overviewOutput);
+        });
+    });
+
+    // Detailed laptop view
     } else if (pathName === '/laptop' && id < laptopData.length) {
         response.writeHead(200, { 'Content-type' : 'text/html' });
-        response.end(`This is the laptop with id ${id}`);
-    } else {
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            response.end(output);
+        });
+
+    // Handle image loading
+    } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            response.writeHead(200, { 'Content-type' : 'image/jpg' });
+            response.end(data);
+        });
+    }
+
+    // If the URL is not found
+    else {
         response.writeHead(404, { 'Content-type' : 'text/html' });
         response.end('Page not found');
     }
-
     
 });
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests');
 });
+
+function replaceTemplate (originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
