664cd828e2524b711d3924ceacfc8d572624ad40
Ryukio <florian.fejer@hotmail.de>
2020-04-20 20:28:48 +0200

    Playing with npm chalk

--- Notes App/app.js
+++ Notes App/app.js
@@ -1,6 +1,8 @@
+const chalk = require('chalk');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
+console.log(chalk.green.bold.inverse('Success!'));
 
 console.log(msg)
