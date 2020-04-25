ee1472b1b26f2f0a51f9027b9c2518d6eb717f05
Ryukio <florian.fejer@hotmail.de>
2020-04-25 23:30:19 +0200

    Add and configure npm yargs

--- Notes App/app.js
+++ Notes App/app.js
@@ -1,13 +1,53 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 // const validator = require('validator');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// Configure yargs by customizing yargs version and adding commands
+yargs.version('1.1.0')
 
-const command = process.argv[2];
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Content of note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log(`Title: ${argv.title} with content: ${argv.body}`);
+    }
+})
 
-if (command === 'add') {
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
 
-} else if (command === 'remove') {
-    
-}
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse();
