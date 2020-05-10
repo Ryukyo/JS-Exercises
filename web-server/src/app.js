7b776c5d5ab2fe08f9d028b492834cf2e5c12d2e
Ryukio <florian.fejer@hotmail.de>
2020-05-10 19:32:26 +0200

    Add sample css, img and switch html to hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,36 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 
+// Setup handlebars engine and set views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static dir 
 app.use(express.static(publicDirectoryPath));
 
-/* app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../public/about.html'))
-})
- 
-app.get('/help', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../public/help.html'))
-}) */
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Florian',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Florian',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'You can do it!',
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send('Check the weather')
